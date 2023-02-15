d78f571c67e6693af6be3af1117bf1edeb9afd2c
asmaasheaba <asmaa.shiba2018@gmail.com>
2023-02-15 11:46:17 +0200

    user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('auth_type',['normal','facebook','google'])->default('normal');
+            $table->string('client_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
